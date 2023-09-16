a1d194392c6b7d532a4db7e413c3307b201832ea
jamesjiayu <jamesjiayu@gmail.com>
2023-09-16 09:28:29 -0500

    4th

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai"
-import { FaLinkedinIn } from "react-icons/fa"
-
+// import { FaLinkedinIn } from "react-icons/fa"
+  // import {
+  //   AiFillGithub,
+  //   AiOutlineTwitter,
+  //   AiFillInstagram,
+  // } from "react-icons/ai"
 function Footer () {
   let date = new Date()
   let year = date.getFullYear()
